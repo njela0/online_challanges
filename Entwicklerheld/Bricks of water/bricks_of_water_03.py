b41b070cdf9d31494cda9e9197d4fd5ed25a8693
Daniela Maier <daniela.maier.90@gmail.com>
2025-05-07 15:33:02 +0200

    water of bricks version 3 - final, but not the result

--- Entwicklerheld/Bricks of water/bricks_of_water_03.py
+++ Entwicklerheld/Bricks of water/bricks_of_water_03.py
@@ -10,6 +10,8 @@ bricksArray9 = [2, 0, 3, 0, 2]
 bricksArray10 = [4, 0, 3, 4, 1, 7, 4, 4, 2, 4]
 bricksArray11 = [3, 2, 3, 0, 3, 2, 0, 0, 0, 3, 0, 3]
 bricksArray12 = [0, 0, 1, 3, 2, 3, 0, 3, 2, 0, 0, 0, 3, 0]
+bricksArray13 = [0, 0, 1, 3, 2, 3, 0, 3, 2, 0, 0, 0, 3, 1, 0]
+bricksArray14 = [0, 0, 1, 0, 3, 2, 3, 0, 3, 2, 0, 0, 0, 3, 1, 0]
 
 def how_much_water(bricks_array: list) -> int:
 
@@ -61,21 +63,43 @@ def how_much_water(bricks_array: list) -> int:
         for element in bricks_array_prepared:
             water_bricks_array.append(max_wall_height - element)
 
-    else:
+    else: # if walls are on the inside
         print("max-walls are on the inside")
         # find the heights of the outer walls
         left_wall_height = bricks_array_prepared[0]
         right_wall_height = bricks_array_prepared[-1]
+        index_start_middle = 0
+        index_end_middle = 0
 
-        # and the amount of water for the outer elements
-        for index, element
+        # amount of water for the elements on the left
+        for index, element in enumerate(bricks_array_prepared):
+            if element < max_wall_height:
+                water_bricks_array.append(left_wall_height - element)
+                print(f"water_bricks_array: {water_bricks_array}")
+            else:
+                index_start_middle = index
+                print(f"index_start_middle: {index_start_middle}")
+                break
 
+        # amount of water for the elements on the right
+        for index in range(len(bricks_array_prepared) - 1, -1, -1):
+            if bricks_array_prepared[index] < max_wall_height:
+                water_bricks_array.append(right_wall_height - bricks_array_prepared[index])
+                print(f"water_bricks_array: {water_bricks_array}")
+            else:
+                index_end_middle = index
+                print(f"index_end_middle: {index_end_middle}")
+                break
 
-        # for element in bricks_array_prepared:
-        #     water_bricks_array.append(max_wall_height - element)
+        # amount of water for the middle part
+        for index, element in enumerate(bricks_array_prepared):
+            if index >= index_start_middle and index < index_end_middle:
+                water_bricks_array.append(max_wall_height - element)
+                print(f"water_bricks_array: {water_bricks_array}")
+
 
     print(f"water_bricks_array: {water_bricks_array}")
     amount_of_water_bricks = sum([x for x in water_bricks_array if x > 0])
     return amount_of_water_bricks
 
-print(how_much_water(bricksArray12))
+print(how_much_water(bricksArray14))

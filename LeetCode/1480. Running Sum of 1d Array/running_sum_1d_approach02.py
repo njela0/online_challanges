6d363ef0e6269c45054855945b5c069cf43ad6b5
Daniela Maier <daniela.maier.90@gmail.com>
2025-05-08 15:30:28 +0200

    running sum second approach - correct

---
+++ LeetCode/1480. Running Sum of 1d Array/running_sum_1d_approach02.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def runningSum(self, nums: list[int]) -> list[int]:
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+
+        running_sum_array = [0] * len(nums)
+        running_sum_array[0] = nums[0]
+
+        for index in range(1, len(nums)):
+
+            running_sum_array[index] = nums[index] + running_sum_array[index - 1]
+
+        return running_sum_array
+
+
+nums1 = [1,2,3,4]
+nums2 = [1,1,1,1,1]
+nums3 = [3,1,2,10,1]
+
+sol1 = Solution()
+print(sol1.runningSum(nums3))

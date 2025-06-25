81312ebb25ec6027118ebe82a1c893df92c15669
Daniela Maier <daniela.maier.90@gmail.com>
2025-06-25 21:46:17 +0200

    Fair Share - Ansatz dict a zu dict b

--- Entwicklerheld/Fair Share/fair_share.py
+++ Entwicklerheld/Fair Share/fair_share.py
@@ -8,14 +8,11 @@ def sum_up_payments(csv_filename):
             entry = line.strip("\n").strip('"').split(';')
             entry[2] = float(entry[2])
 
-            if entry[1] not in people_dict.values():
-                people_dict["person"] = entry[1]
-                people_dict["total"] = entry[2]
-                people_dict["refund"] = 1
-
+            if entry[1] not in people_dict.keys():
+                people_dict[entry[1]] = [entry[2], 1]
             else:
-                people_dict["total"] += entry[2]
-                people_dict["refund"] += 1
+                people_dict[entry[1]][0] += entry[2]
+                people_dict[entry[1]][1] += 1
 
     return people_dict, amount_of_activities
 
@@ -30,5 +27,3 @@ print(amount_of_activities)
 for key, value in people_dict.items():
     print(key, value)
 
-if [154.0, 4] in people_dict.values():
-    print("No more shares")

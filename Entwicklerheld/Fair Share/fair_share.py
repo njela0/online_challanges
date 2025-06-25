1e3fa2ae606234afee4a7a15bf71461b9023078d
Daniela Maier <daniela.maier.90@gmail.com>
2025-06-25 22:43:58 +0200

    Fair Share - funktionsfähig, noch nicht eingereicht

--- Entwicklerheld/Fair Share/fair_share.py
+++ Entwicklerheld/Fair Share/fair_share.py
@@ -1,29 +1,44 @@
 def sum_up_payments(csv_filename):
-    people_dict = {}
-    amount_of_activities = 0
+    """Takes input from csv file and returns a dictionary with the total and parts per person and a variable containing the number of participants"""
+    shares_dict = {}
+    total_payment = 0
+    number_of_participants = 0
+
     with open(csv_filename, "r") as data:
 
         for line in data:
-            amount_of_activities += 1
             entry = line.strip("\n").strip('"').split(';')
             entry[2] = float(entry[2])
+            total_payment += entry[2]
 
-            if entry[1] not in people_dict.keys():
-                people_dict[entry[1]] = [entry[2], 1]
+            if entry[1] not in shares_dict.keys():
+                shares_dict[entry[1]] = entry[2]
+                number_of_participants += 1
             else:
-                people_dict[entry[1]][0] += entry[2]
-                people_dict[entry[1]][1] += 1
+                shares_dict[entry[1]] += entry[2]
 
-    return people_dict, amount_of_activities
+    return shares_dict, total_payment, number_of_participants
 
 
-def calculate_shares(people_dict, amount_of_activities):
-    pass
+def calculate_shares(shares_dict, total_payment, number_of_participants):
+    shares_list = []
+
+    for key, value in shares_dict.items():
+        person_shares_dict = {}
+        person_shares_dict["people"] = key
+        person_shares_dict["total"] = value
+        person_shares_dict["refund"] = round(((total_payment / number_of_participants) - value), 2)
+        shares_list.append(person_shares_dict)
+
+    return shares_list
 
 csv_filename = "data-1-1.csv"
-people_dict, amount_of_activities = sum_up_payments(csv_filename)
+shares_dict, total, number_of_participants = sum_up_payments(csv_filename)
 
-print(amount_of_activities)
-for key, value in people_dict.items():
+print(number_of_participants)
+for key, value in shares_dict.items():
     print(key, value)
 
+shares_list = calculate_shares(shares_dict, total, number_of_participants)
+
+print(shares_list)
